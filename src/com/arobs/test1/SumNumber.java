8592918a1952cb4d8d2caf17a94dc25d5d696d84
Scridon Darius <49202131+ScridonDarius@users.noreply.github.com>
2020-01-30 10:41:24 +0200

    SumNumbers

---
+++ src/com/arobs/test1/SumNumber.java
@@ -0,0 +1,15 @@
+package com.arobs.test1;
+
+public class SumNumber {
+
+    public static int sumNumbers(int a, int b) {
+
+        int sum = 0;
+        return sum = a + b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(sumNumbers(4,5));
+    }
+}
