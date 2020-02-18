7ee4364b5766a8d9d37887d2857518066f5be75f
Scridon Darius <49202131+ScridonDarius@users.noreply.github.com>
2020-02-18 12:28:40 +0200

    Create Project With SpringBoot

---
+++ App/library-management/src/test/java/com/arobs/internship/librarymanagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arobs.internship.librarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
