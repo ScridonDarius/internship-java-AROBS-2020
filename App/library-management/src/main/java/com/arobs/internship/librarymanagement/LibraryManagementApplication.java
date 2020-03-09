3e146152d80d861045a2482e7b9b8bdb61a1ec8f
Scridon <scridondarius25@gmail.com>
2020-03-09 11:51:34 +0200

    Email Configuration

--- App/library-management/src/main/java/com/arobs/internship/librarymanagement/LibraryManagementApplication.java
+++ App/library-management/src/main/java/com/arobs/internship/librarymanagement/LibraryManagementApplication.java
@@ -3,8 +3,10 @@ package com.arobs.internship.librarymanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+@EnableScheduling
 public class LibraryManagementApplication {
 
     public static void main(String[] args) {
